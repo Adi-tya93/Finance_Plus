52e7b076a730f1b42c1b12c16269665b9a08a97a
Aditya <sharmadityaa@gmail.com>
2020-07-04 18:24:23 -0400

    Changed GET to POST in login forms

--- login/views.py
+++ login/views.py
@@ -10,8 +10,8 @@ def index(request):
 
 
 def validate(request):
-    user_name = request.GET['username']
-    password = request.GET['password']
+    user_name = request.POST['username']
+    password = request.POST['password']
     result = User.objects.login_user(
         name=user_name, password=password)
     return render(request, 'login/validate.html', {'username': user_name, 'password': password, "result": result})
@@ -22,9 +22,9 @@ def register(request):
 
 
 def adduser(request):
-    new_user = request.GET['newuser']
-    new_password = request.GET['newpassword']
-    confirm_password = request.GET['confirmpassword']
+    new_user = request.POST['newuser']
+    new_password = request.POST['newpassword']
+    confirm_password = request.POST['confirmpassword']
     result = User.objects.create_user(
         name=new_user, password=new_password, confirm_password=confirm_password)
     return render(request, 'login/adduser.html', {'newuser': new_user, 'newpassword': new_password, "result": result[0], "match": result[1]})
