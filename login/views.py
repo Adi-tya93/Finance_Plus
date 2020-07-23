9efde589133ac85157c98bb96c2e33d5c692e13f
Aditya <sharmadityaa@gmail.com>
2020-07-23 14:14:55 -0400

    Passwords are hased

--- login/views.py
+++ login/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import User
+import hashlib
 
 # Create your views here.
 
@@ -12,6 +13,8 @@ def index(request):
 def validate(request):
     user_name = request.POST['username']
     password = request.POST['password']
+    m = hashlib.md5(bytes(password, "ascii"))
+    password = m.hexdigest()
     result = User.objects.login_user(
         name=user_name, password=password)
     return render(request, 'login/validate.html', {'username': user_name, 'password': password, "result": result})
@@ -25,6 +28,10 @@ def adduser(request):
     new_user = request.POST['newuser']
     new_password = request.POST['newpassword']
     confirm_password = request.POST['confirmpassword']
+    m = hashlib.md5(bytes(new_password, "ascii"))
+    new_password = m.hexdigest()
+    m = hashlib.md5(bytes(confirm_password, "ascii"))
+    confirm_password = m.hexdigest()
     result = User.objects.create_user(
         name=new_user, password=new_password, confirm_password=confirm_password)
     return render(request, 'login/adduser.html', {'newuser': new_user, 'newpassword': new_password, "result": result[0], "match": result[1]})
